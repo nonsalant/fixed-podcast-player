c8c7469cf7254d002eebc941272848bf90fc2941
Stefan Matei <nonsalant@gmail.com>
2025-03-04 18:10:51 +0200

    refactor handlers in _customizer.js

--- customizer/global-this/_customizer.js
+++ customizer/global-this/_customizer.js
@@ -17,41 +17,53 @@ globalThis.customizer.setupContentListener = class {
     constructor(el, attName) {
         const kebabToCamel = (str) => str.replace(/-./g, (match) => match.charAt(1).toUpperCase());
         const methodName = kebabToCamel(attName);
-        const nonContentAtts = ["data-variation", "data-position"];
+        const simpleOptionAtts = ["data-variation", "data-position"];
+        const isSimple = simpleOptionAtts.includes(attName);
 
-        if (nonContentAtts.includes(attName)) new.target.setupAttListener(el, attName);
-        else new.target[methodName](el, attName);
+        if (isSimple) {
+            new.target.setupAttListener(el, attName);
+        }
+        else {
+            new.target[methodName](el, attName);
+        }
     }
 
     static commonCode(attName) {
         return `
             const pp = document.querySelector('podcast-player');
+            const $ = (sel) => pp.shadow.querySelector(sel);
             pp.setAttribute('${attName}', this.value);
-            pp.shadowRoot.querySelector('.podcast-player').setAttribute('${attName}', this.value);
         `;
     }
 
     static setupAttListener(el, attName) {
-        el.setAttribute("oninput", this.commonCode(attName));
+        el.setAttribute("oninput", this.commonCode(attName) + `
+            const div = $('.podcast-player');
+            div.setAttribute('${attName}', this.value);
+        `);
     }
 
     static dataTitle(el, attName) {
         el.setAttribute("oninput", this.commonCode(attName) + `
-            pp.shadowRoot.querySelector('h3').setAttribute('title', this.value);
-            pp.shadowRoot.querySelector('h3').innerText = this.value;
+            const h3 = $('h3');
+            h3.setAttribute('title', this.value);
+            h3.innerText = this.value;
         `);
     }
 
     static dataThumb(el, attName) {
         el.setAttribute("oninput", this.commonCode(attName) + `
-            pp.shadowRoot.querySelector('header').style.backgroundImage = 'url('+this.value+')';
+            const header = $('header');
+            header.style.backgroundImage = 'url('+this.value+')';
         `);
     }
 
     static dataSrc(el, attName) {
         el.setAttribute("oninput", this.commonCode(attName) + `
-            pp.shadowRoot.querySelector('audio').src = this.value;
-            pp.shadowRoot.querySelector('a')?.setAttribute('href', this.value);
+            const audio = $('audio');
+            const a = $('a');
+            audio.src = this.value;
+            a?.setAttribute('href', this.value);
         `);
     }
     
